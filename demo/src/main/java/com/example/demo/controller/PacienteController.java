afdf346b4e2d0fa4b7231f1d2e87c11a46e06bc6
felipe-meza-mora <feli.meza@duocuc.cl>
2024-04-27 20:44:12 -0400

    Ultimo comit
    Corrección de dependencias no utilizadas

--- demo/src/main/java/com/example/demo/controller/PacienteController.java
+++ demo/src/main/java/com/example/demo/controller/PacienteController.java
@@ -28,7 +28,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
--- demo/src/test/java/com/example/demo/Service/PacienteServiceTest.java
+++ demo/src/test/java/com/example/demo/Service/PacienteServiceTest.java
@@ -2,7 +2,6 @@ package com.example.demo.Service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
